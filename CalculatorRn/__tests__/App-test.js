3c956c1b508aa4c0a08f11c1a55824639bd52321
jshahriyarbadalov <shah.badalov91@gmail.com>
2019-12-21 18:03:56 +0400

    Write Unity Test
    Unit Test

--- CalculatorRn/__tests__/App-test.js
+++ CalculatorRn/__tests__/App-test.js
@@ -15,6 +15,40 @@ describe('Test my function btnPressed ', () => {
     const myfunction = renderer.create(<App />).toJSON
     expect(myfunction).toMatchSnapshot();
   })
+  it('valid function Testing App Component', () => {
+    let rgx = renderer.create(<App />).getInstance()
+    let variable = rgx.validate()
+    expect(variable).toBeDefined()
 
+  })
+  it('calculateResult function  Testing App Component', () => {
+    let clc = renderer.create(<App />).getInstance()
+    let variab = clc.calculateResult()
+    expect(variab).not.toBeDefined()
+
+  })
+  it('constructor function  Testing App Component', () => {
+    let ctr = renderer.create(<App />).getInstance()
+    let vrb = ctr.constructor()
+    expect(vrb).toBeDefined()
+
+  })
+  it('btnPressed function  Testing App Component', () => {
+    let oper = "="
+    let result='='
+    let btn = renderer.create(<App />).getInstance()
+
+    expect(btn.btnPressed(oper)).not.toEqual(result)
+
+  })
+
+  it('calculator function  Testing App Component', () => {
+    let oper = "+"
+    let result="+"
+    let ops = renderer.create(<App />).getInstance()
+
+    expect(ops.calculator(oper)).toBeDefined()
+
+  })
 });
 
